8324d652d0168d1496183145508cb9381f2e6ed3
TAKANO Sho / @masaru_b_cl <masaru.b.cl@gmail.com>
2013-03-24 00:33:15 +0900

    get by unset key : make test

---
+++ LTSVTest/LTSVTest.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+
+namespace LTSVTest
+{
+  [TestClass]
+  public class LTSVTest
+  {
+    [TestMethod]
+    public void 存在しないキーで取得すると値はnull()
+    {
+      var ltsv = new LTSV();
+      string value = ltsv.Get("key");
+
+      value.IsNull();
+    }
+  }
+}
